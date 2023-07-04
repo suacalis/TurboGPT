82bdd2e3fbe1c177e341bec04df86b2c80b88e50
tushar2704 <tushar.inseec@gmail.com>
2023-07-04 16:20:35 +0530

    update

--- TurboGPT.py
+++ TurboGPT.py
@@ -13,6 +13,7 @@ st.title("🤖TurboGPT")
 st.markdown("### By [Tushar Aggarwal](https://www.tushar-aggarwal.com/)")
 
 # Initialize OpenAI API
+
 st.sidebar.markdown("## OpenAI API Key")
 
 api_key = st.sidebar.text_input("Enter your OpenAI API key")
@@ -28,6 +29,50 @@ if "openai_model" not in st.session_state:
 
 
 #initializing chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    
+#display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+        
+#react to user input
+prompt = st.chat_input("How can i help you today?")
+
+if prompt:
+    #display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    #adding user message to chat history
+    st.session_state.messages.append({"role":"user", "content":prompt})
+    
+    with st.chat_message("assistant"):
+        message_placeholder=st.empty()
+        full_response=""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role":m["role"], "content":m['content']}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response+=response.choices[0].delta.get("content","")
+            message_placeholder.markdown(full_response +"")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role":"assistant", "content":full_response})
+    
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 
